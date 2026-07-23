efb8f5187a7afb2d9bd53ea03ef8951607f76c69
Symeyrie <contact.sharedorbit@gmail.com>
2026-07-23 17:17:29 +0300

    Add Shared Orbit logo to home page

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,23 @@
+import { h } from 'vue'
+import { useData, withBase } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  Layout() {
+    const { frontmatter } = useData()
+
+    return h(DefaultTheme.Layout, null, {
+      'layout-top': () => {
+        if (frontmatter.value.layout !== 'home') return null
+
+        return h('img', {
+          class: 'shared-orbit-home-logo',
+          src: withBase('/shared-orbit-icon.png'),
+          alt: 'Shared Orbit'
+        })
+      }
+    })
+  }
+}
